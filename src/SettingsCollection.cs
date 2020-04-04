4c679d2e68f90d030932fd6c8673d8fee3f7ca39
Yuuki Wesp <ls-micro@ya.ru>
2020-04-04 04:20:36 +0300

    rm unused files

--- src/SettingsCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ivy.Versioning
-{
-    using System.Collections.Generic;
-
-    public class SettingsCollection : Settings
-    {
-        public Dictionary<string, Settings> Configurations { get; set; }
-    }
-}
